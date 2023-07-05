1b51fc8942ce89468d084fc430f1b49b68d9ed17
hernan6776 <hernanferrero1@gmail.com>
2023-07-05 17:46:45 -0300

    subida carpeta 5

---
+++ clase 6 - operadores y exp regulares/repaso variables clase 5.js
@@ -0,0 +1,13 @@
+
+// VAR al tener un alcance global se imprime tanto en el bloque interno como externo:
+{
+    var activo = true;
+    console.log(activo);
+}
+console.log(activo);
+
+// LET alcance de bloque, no permite redifinicion en el mismo bloque
+
+
+
+
