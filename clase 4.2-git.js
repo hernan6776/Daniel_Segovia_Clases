6d808eaab3683f315f7a38aed5b4a0450ed0c867
hernan6776 <hernanferrero1@gmail.com>
2023-07-05 10:39:43 -0300

    subida a carpeta 4

--- clase 4.2-git.js
+++
@@ -1,7 +0,0 @@
-//pongo este nuevo comentario en main
-// hola como estas? rama1
-//aca tambien coloco este nuevo comentario en main
-// bien y vos? rama 1
-// agrego un nuevo comentario - rama 2
-// soy ramaA y voy a escribir otra cosaf
-// soy ramaB y estoy subiendo algo
